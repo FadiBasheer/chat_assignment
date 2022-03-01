81a02817598f066ac3dabd40111b325a279af4d1
FadiBasheer <fadih1992@gmail.com>
2022-03-01 09:35:01 -0800

    Initial commit

---
+++ src/Play.c
@@ -0,0 +1,44 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+static void record(int fd);
+
+static void play(int fd);
+
+int main(int argc, char **argv) {
+    int pipe_fds[2];
+    pid_t pid;
+
+    pipe(pipe_fds);
+    pid = fork();
+
+    if (pid == 0) {
+        close(pipe_fds[1]);
+        play(pipe_fds[0]);
+    } else if (pid > 0) {
+        close(pipe_fds[0]);
+        record(pipe_fds[1]);
+    }
+    return EXIT_SUCCESS;
+}
+
+static void play(int fd) {
+    char *cmd = "aplay -";
+    char buf[256];
+    FILE *fp = popen(cmd, "w");
+
+    while (true) {
+        ssize_t nread;
+        nread = read(fd, buf, sizeof(buf));
+        printf("%zu\n", nread);
+
+        if (nread > 0) {
+            fwrite(buf, 1, sizeof(buf), fp);
+        } else {
+            break;
+        }
+    }
+    pclose(fp);
+}
